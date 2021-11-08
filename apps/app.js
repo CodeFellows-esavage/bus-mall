ac0637717237c08cbaf40b299a3a51bec36cde40
erik savage <erik.k.savage@gmail.com>
2021-11-08 14:56:40 -0800

    added constructor functions and created objects

--- apps/app.js
+++ apps/app.js
@@ -1,2 +1,45 @@
 'use strict';
 // console.log('linked');
+
+function Product(productName, fileExt) {
+    this.productName = productName;
+    this.imgPath = `imgs/${productName}.${fileExt}`;
+    this.countShown = 0;
+    this.countClicked = 0;
+
+    Product.productList.push(this);
+}
+
+Product.productList = [];
+
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+let index = 0;
+
+function imgtest() {
+    img1.src = Product.productList[index].imgPath;
+    index += 1;
+}
+
+const bag = new Product('bag', 'jpg');
+const banana = new Product('banana', 'jpg');
+const bathroom = new Product('bathroom', 'jpg');
+const boots = new Product('boots', 'jpg');
+const breakfast = new Product('breakfast', 'jpg');
+const bubblegum = new Product('bubblegum', 'jpg');
+const chair = new Product('chair', 'jpg');
+const cthulhu = new Product('cthulhu', 'jpg');
+const dogduck = new Product('dog-duck', 'jpg');
+const dragon = new Product('dragon', 'jpg');
+const pen = new Product('pen', 'jpg');
+const petsweep = new Product('pet-sweep', 'jpg');
+const scissors = new Product('scissors', 'jpg');
+const shark = new Product('shark', 'jpg');
+const sweep = new Product('sweep', 'png');
+const tauntaun = new Product('tauntaun', 'jpg');
+const unicorn = new Product('unicorn', 'jpg');
+const watercan = new Product('water-can', 'jpg');
+const wineglass = new Product('wine-glass', 'jpg');
+
+img1.addEventListener('click', imgtest);
